5db05a0f46bd60669868b6b9091716dae1e5cb19
Phil Bayfield <phil@bayfmail.com>
2010-05-01 12:32:01 +0100

    add basic structure

---
+++ smpp.go
@@ -0,0 +1,35 @@
+/**
+ * GoSMPP - An SMPP library for Go
+ * Copyright 2010 Phil Bayfield
+ * This software is licensed under a Creative Commons Attribution-Share Alike 2.0 UK: England & Wales License
+ * Further information on this license can be found here: http://creativecommons.org/licenses/by-sa/2.0/uk/
+ */
+package smpp
+
+/**
+ * Create a new Transmitter
+ */
+func NewTransmitter(host string, port int, username, password string) {
+
+}
+
+/**
+ * Create a new Receiver
+ */
+func NewReceiver(host string, port int, username, password string) {
+
+}
+
+/**
+ * Create a new Transceiver
+ */
+func NewTransceiver(host string, port int, username, password string) {
+
+}
+
+/**
+ * Create a new Server
+ */
+func NewServer() {
+
+}
