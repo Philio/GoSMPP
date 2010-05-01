0afdc609756dddb73b6e78a60a623e2fbe0ee122
Phil Bayfield <phil@bayfmail.com>
2010-05-01 12:32:58 +0100

    added other files

---
+++ smpp_transmitter.go
@@ -0,0 +1,7 @@
+/**
+ * GoSMPP - An SMPP library for Go
+ * Copyright 2010 Phil Bayfield
+ * This software is licensed under a Creative Commons Attribution-Share Alike 2.0 UK: England & Wales License
+ * Further information on this license can be found here: http://creativecommons.org/licenses/by-sa/2.0/uk/
+ */
+package smpp
